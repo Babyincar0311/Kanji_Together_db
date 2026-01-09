4c6c5f95c4edfb0181a05ad785ee78da2db7d36d
Babyincar0311 <phamgiakhang0311@gmail.com>
2026-01-09 15:34:27 +0700

    Feature: Create basic CRUD for Clazz

---
+++ src/main/java/org/t2404e/kanji_together_db/dto/ErrorResponse.java
@@ -0,0 +1,15 @@
+package org.t2404e.kanji_together_db.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+}
